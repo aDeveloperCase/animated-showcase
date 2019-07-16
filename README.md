16478da4673d233a6a177281195e0c91ab7acb9a
robi <rroberto2019@outlook.com>
2019-07-16 17:42:50 +0200

    upload project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# animated-showcase
+# showcase
+A [showcase](http://aDeveloperCase.github.io/project-showcase/) experiment created for my old website.
+It can be used to show projects, photos or other medias.
+It works better with chrome.
---
+++ js/app.js
@@ -0,0 +1,464 @@
+/**
+*	@author qrob | https://github.com/qrob
+*/
+
+var projects = {projects: [
+	{
+		url: "http://lorempixel.com/400/420",
+		github: "https://github.com/myrepo1010/nanodegree/tree/master/P6%20-%20Feed%20Reader",
+		tags: ["JS", "TDD", "Jasmine"],
+		title: "Nanodegree - Feed Reader",
+		type: "launch",
+		date: "2014",
+		imgUrl: "http://lorempixel.com/400/420",
+		description: "Tests written with Jasmine for a feed reader application.",
+		status: "maintenance"
+	},
+	{
+		url: "http://lorempixel.com/400/430",
+		github: "https://github.com/myrepo1010/nanodegree/tree/master/P3%20-%20Arcade%20Game",
+		tags: ["HTML5", "JS"],
+		title: "Nanodegree - Arcade Game",
+		type: "launch",
+		date: "2014",
+		imgUrl: "http://lorempixel.com/400/430",
+		description: "Frogger like game in HTML5",
+		status: "development"
+	},
+	{
+		url: "http://lorempixel.com/400/440",
+		github: "https://github.com/myrepo1010/nanodegree/tree/master/P2%20-%20Interactive%20Resume",
+		tags: ["JS"],
+		title: "Nanodegree - Interactive Resume",
+		type: "launch",
+		date: "2014",
+		imgUrl: "http://lorempixel.com/400/440",
+		description: "A resume builder",
+		status: "maintenance"
+	},
+	{
+		url: "http://lorempixel.com/400/450",
+		github: "https://github.com/myrepo1010/nanodegree/tree/master/P1%20-%20Mockup%20to%20Website",
+		tags: ["HTML"],
+		title: "Nanodegree - Mockup to Website",
+		type: "Launch",
+		date: "2014",
+		imgUrl: "http://lorempixel.com/400/450",
+		description: "",
+		status: "maintenance"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+	{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+		{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	},
+		{
+		url: "http://lorempixel.com/400/460",
+		github: "",
+		tags: ["PHP"],
+		title:"Availability Calendar",
+		type: "Launch",
+		date:"2014",
+		imgUrl: "http://lorempixel.com/400/460",
+		description: "A calendar generator written in PHP, where you can mark busy days and store notes through a JS based interface.",
+		status: "inactive"
+	}
+]};
+
+var projectsRun = function() {
+
+//require(['snap'], function(snap){
+	//$('loading').addClass('active');
+
+	var qbox = {
+		hexanet 	: {},
+		paper 		: {},
+		container 	: {width: 650, height: 500},
+		preview 	: {height: 250 + 20},
+		hexagon 	: {height: 160, width: 200, center: 100, spike: 50}
+	};
+	qbox.compute = function() {
+		var num = projects.projects.length;
+		this.hexanet.height = ((num % 2) + Math.floor(num / 2)) * (this.hexagon.height / 2) + (this.hexagon.height / 2);
+		this.hexanet.width = (this.hexagon.width * 2) + (this.hexagon.center * 2) + this.hexagon.spike;
+
+		this.container.x = $('#portfolio').offset().left;
+		this.container.y = $('#portfolio').offset().top;
+
+		this.paper.height = this.hexanet.height + this.preview.height;
+		this.paper.width = this.hexanet.width;
+		this.paper.topOverflow = this.preview.height;
+		this.paper.bottomOverflow = this.paper.height - (this.container.height + this.preview.height);
+	};
+	qbox.compute();
+
+	var svgContainer = Snap(650,500).attr({id: "qbox"});
+	$('#portfolio').append(svgContainer.node);
+
+	var s = svgContainer.svg(0,- qbox.preview.height, qbox.paper.width, qbox.paper.height);
+	s.data = {};
+
+	var selector, set, grayFilter = s.filter(Snap.filter.grayscale(1)), blurFilter = s.filter(Snap.filter.blur(2,2));
+	qbox.display = function() {
+		var length = projects.projects.length;
+
+		set = Snap.set();
+
+		var offset=0, colNum = 0, colMax = 2, x=-300, y=350;
+		for(var i=0; i<length; i++){
+			var p = projects.projects[i];
+			p.index = i;
+
+			if(colNum === colMax){
+				//colMax = (colMax === 3) ? 2 : 3;
+				offset = (offset === 150) ? 0 : 150;
+				colNum = 0;
+
+				x  = 0 + offset;
+				y += 80;
+			}else{
+				x += 300;
+			}
+
+			colNum++;
+
+
+			var image = s.image(p.imgUrl, x, y-80, 200, 160);
+			/* for Interet Explorer*/
+			if(image.attr("preserveAspectRatio") === "none meet")
+				image.attr({x: 0, y: 0});
+
+			var pattern = image.toPattern(x, y-80, 200, 160);
+
+			var pathString = "M"+x+","+y+" l50,-80 l100,0 l50,80 l-50,80 l-100,0Z";
+			var backHex = s.path(pathString).attr({
+				fill: "#333",
+				stroke: "#111",
+				strokeWidth: 2,
+				opacity: 0
+			});
+			var frontHex = s.path(pathString).attr({
+				fill: pattern,
+				stroke: "#111",
+				strokeWidth: 2,
+				filter: grayFilter,
+				opacity: 0
+			});
+			backHex.animate({opacity: 1}, 500, mina.easeout);
+			frontHex.animate({opacity: 1}, Math.floor(Math.random() * (2000 - 800)) + 400, mina.easeout)
+			//var hexagon = s.group(pattern, backHex, frontHex);
+			frontHex.data = {
+				index: i,
+				x: x,
+				y: y
+			};
+			zIndex.last = frontHex;
+			frontHex.hover(hexagonAnm.onOver, hexagonAnm.onOut, frontHex, frontHex);
+			frontHex.click(hexagonAnm.onClick);
+			frontHex.addClass("anm3 hexagon");
+			frontHex.attr({id: "hex-" + i});
+
+			set.push(backHex, frontHex);
+		}
+
+		$('loading').removeClass('active');
+	};
+
+	var zIndex = {
+		last: null,
+		bringFront: function(el) {
+			this.last.after(el);
+		}
+	};
+
+	var preview = {
+		hexagon: null,
+		previewImg: null,
+		show: function(hex) {
+			this.hexagon = hex;
+			var p = projects.projects[hex.data.index];
+
+			var pattern = s.image(p.imgUrl, 0, 0, 300, 250).toPattern(0, 0, 300, 250);
+			//"M0,125 75,0 225,0 300,125 225,250 75,250Z"
+			this.previewImg = s.path("M0,127 l75,-125 l150,0 l75,125 l-75,125 l-150,0Z").attr({
+				fill: pattern,
+				stroke: "#0665FF",
+				strokeWidth: 2
+			}).addClass("anm3");
+
+			var HTMLproject = "<h3 id='title' class='preview-element'>"+p.title+" - "+p.date+"</h3>"+
+				"<div id='description' class='preview-element'>"+((p.description) ? p.description : "No description available")+"</div>"+
+				"<div id='status'><strong>Status: </strong> "+((p.status) ? p.status : "unknown")+"</div>"+
+				((p.tags) ? "<div id='tags'><strong>Tags:</strong> " + p.tags + "</div>" : "")+
+				"<div id='preview-buttons' class='anm3'>"+
+					"<a id='url' class='preview-element button' target='_blank' href='"+p.url+"'>"+p.type.toUpperCase()+"</a>"+
+					((p.github) ? "<a id='github' class='preview-element button' target='_blank' href='"+p.github+"'>GITHUB</a>" : "")+
+					"<a id='ejectBtn' class='preview-element button' href='javascript:'>EJECT</a>"+
+				"</div>";
+
+			$('#preview').html(HTMLproject).removeClass("hide transparent");
+			$('#ejectBtn').click(preview.remove);
+		},
+		remove: function() {
+			preview.previewImg.addClass("transparent");
+			$('#preview').addClass("hide transparent");
+
+			s.animate({y: s.data.lastY}, 400, mina.easeout, function() {
+				preview.previewImg.remove();
+				$('#preview').html("");
+				s.data.locked = false;
+				preview.hexagon.attr({filter: grayFilter});
+				//preview.hexagon.removeClass("transparent");
+				preview.hexagon.attr({opacity: 1});
+			});
+		}
+	};
+
+	var hexagonAnm = {
+		onOver: function() {
+			if(!$('#preview').hasClass("hide"))
+				return;
+
+			var hex = this;
+			hex.attr({filter: null});
+			$("#title-over").html(" - "+projects.projects[hex.data.index].title).removeClass('transparent');
+			selector.attr({opacity: 0, path: "M"+hex.data.x+","+hex.data.y+" l50,-80 l100,0 l50,80 l-50,80 l-100,0Z"});
+			selector.animate({opacity: 1}, 400, mina.easing);
+		},
+		onOut : function() {
+			if(!$('#preview').hasClass("hide"))
+				return;
+
+			$("#title-over").addClass('transparent');
+			var hex = this;
+			hex.attr({filter: grayFilter});
+			selector.animate({opacity: 0}, 400, mina.easing);
+		},
+		onClick: function() {
+			if(!$('#preview').hasClass("hide"))
+				return;
+			s.data.locked = true;
+			var hex = this;
+			var matrix = Snap.matrix();
+			matrix.translate( - (hex.data.x - 50), (2*(hex.data.y)) + (130 - hex.data.y));
+			matrix.scale(1,-1);
+
+			selector.animate({opacity: 0}, 100, mina.easing);
+
+			var clone = hex.clone().attr({
+				fill: "#0665FF",
+				filter: blurFilter
+			});
+
+			zIndex.bringFront(clone);
+
+			//hex.addClass("transparent");
+			hex.attr({opacity: 0});
+
+			s.data.lastY = s.attr("y");
+			s.stop();
+			s.animate({y: 0}, 800, mina.easeout);
+
+			var pathString = Snap.path.map(this.attr("path"), matrix).toString();
+			clone.animate({path: pathString, fill: "#0665FF"}, 500, mina.easeout, function() {
+				preview.show(hex);
+				clone.remove();
+			});
+		}
+	};
+
+	var paperAnm = {
+		mouseOver: function(e) {
+			if(s.data.locked)
+				return;
+
+			var mY = e.pageY - qbox.container.y,
+				pY1 = - qbox.paper.topOverflow + 150,
+				pY2 = - (qbox.paper.topOverflow + qbox.paper.bottomOverflow) - 150,
+				mY1 = 0,
+				mY2 = qbox.container.height;
+
+			var pY = ((pY2 - pY1)/(mY2 - mY1)) * (mY - mY1) + pY1;
+
+			s.animate({y: pY}, 900, mina.backout);
+		},
+		mouseOut: function() {
+			if(s.data.locked)
+				return;
+
+			s.stop();
+			s.animate({y: (- qbox.paper.topOverflow)}, 900, mina.backout);
+		}
+	};
+
+	$('#portfolio').mousemove(paperAnm.mouseOver);
+	$('#portfolio').mouseout(paperAnm.mouseOut);
+
+	qbox.display();
+
+	selector = s.path("M-200,80 l50,-80 l100,0 l50,80 l-50,80 l-100,0Z").attr({
+		stroke: "#0665FF",
+		fill: "none",
+		strokeWidth: 2
+	});
+
+	$('#filter-tool').on('keyup', function(e) {
+		var key = e.keyCode || e.which;
+
+        if(key == 13) {
+        	return;
+        }
+        var string = $(this).val().toLowerCase();
+
+        if(!string.length){
+        	s.selectAll('.hexagon').forEach(function(hex) {
+        		hex.removeClass('transparent');
+        	});
+        	return;
+        }
+
+        s.selectAll('.hexagon').forEach(function(hex) {
+        	var p = projects.projects[hex.data.index];
+        	var success = false;
+        	if(p.title.toLowerCase().search(string) != -1 || p.description.toLowerCase().search(string) != -1)
+        		success = true;
+
+        	p.tags.forEach(function(tag) {
+        		if(tag.toLowerCase().search(string) != -1)
+        			success = true;
+        	});
+        	(!success) ? hex.addClass('transparent') : hex.removeClass('transparent');
+        });
+	});
+//});
+
+};
+
+projectsRun();
